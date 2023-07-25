6351f8315fbc3b272954ae3099bc1a185fe27648
adilaytanofficial <adilaytanofficial@gmail.com>
2023-07-25 19:59:31 +0300

    Adding Note Model and MysqlDbHelper Class

---
+++ Demo Project/Model/Note.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Demo_Project.Model
+{
+    public class Note
+    {
+        
+        private String title;
+        private String description;
+        private int id;
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Title { get => title; set => title = value; }
+
+        [Required]
+        [StringLength(200, MinimumLength = 3)]
+        public string Description { get => description; set => description = value; }
+        public int Id { get => id; set => id = value; }
+    }
+}
---
+++ Demo Project/MySqlDbHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Demo_Project.Model;
+using MySql.Data.MySqlClient;
+
+namespace Demo_Project
+{
+    public class MySqlDbHelper
+    {
+
+        private MySqlConnection connection;
+        private String currentServer;
+        private String currentDatabase;
+        private String currentUserId;
+        private String currentUserPassword;
+
+
+        private String getConnectionString()
+        {
+            return "SERVER=" + currentServer + ";" + "DATABASE=" + currentDatabase + ";" + "UID=" + currentUserId + ";" + "PASSWORD=" + currentUserPassword + ";";
+        }
+
+        public MySqlDbHelper(String server, String database, String userId, String userPassword)
+        {
+            currentServer = server;
+            currentDatabase = database;
+            currentUserId = userId;
+            currentUserPassword = userPassword;
+            connection = new MySqlConnection(getConnectionString());
+        }
+
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch 
+            {
+                return false;
+            }
+        }
+
+        public bool AddNote(int userId, Note note)
+        {
+            return true;
+        }
+
+
+    }
+}
